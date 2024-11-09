29c40b9a09f18595fb1c953c2269f2c72723b5ec
Chris Annunziato <NeonMeteors@gmail.com>
2024-11-09 15:46:39 -0500

    added bars randomly

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,24 +1,28 @@
 const Body = () => {
+    const arr = [];
+    for (let i = 0; i < 65; i++) {
+        arr.push(<Bar key={i}/>);
+    }
+
     return (
-        <section className="h-full w-full border ">
-        <div className="flex border border-2">
+        <section className="border h-full w-full ">
+            <div className="flex gap-1 items-end">
 
-            <Bars/>
+                {arr}
 
-
-        </div>
+            </div>
         </section>
     )
 }
 
-const Bars = () => {
-    return(
-        <div>
-            {
+const Bar = () => {
+    const randInt = Math.floor(Math.random() * 250)
+    const minHeight = 10
+    const height = `${minHeight + randInt}px`
 
-
-
-            }
+    return (
+        <div className="rounded-sm w-[6px] h-96 bg-blue-500 "
+             style={{ height: height }}>
         </div>
     )
 }
