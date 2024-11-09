aeed5c53c5d45f2e49de2dce459b8b613d95d8d6
Chris Annunziato <NeonMeteors@gmail.com>
2024-11-09 13:38:39 -0500

    general structure setup

---
+++ src/components/Body.tsx
@@ -0,0 +1,27 @@
+const Body = () => {
+    return (
+        <section className="h-full w-full border ">
+        <div className="flex border border-2">
+
+            <Bars/>
+
+
+        </div>
+        </section>
+    )
+}
+
+const Bars = () => {
+    return(
+        <div>
+            {
+
+
+
+            }
+        </div>
+    )
+}
+
+
+export default Body
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,16 @@
+import Header from "../components/Header.tsx";
+import Body from "../components/Body.tsx";
+import Footer from "../components/Footer.tsx";
+
+const HomePage = () => {
+    return (
+        <>
+            <Header/>
+            <Body/>
+            <Footer/>
+        </>
+    )
+}
+
+
+export default HomePage
