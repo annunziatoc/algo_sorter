d8b9487506675f1373a4747ac034874d0640e28a
Chris Annunziato <cmannunziato@gmail.com>
2025-02-24 16:01:06 -0500

    completed magnetic movement component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import HomePage from "./pages/HomePage.tsx";
+import {BrowserRouter} from "react-router-dom";
 
 function App() {
    return (
-   <HomePage/>
+       <BrowserRouter>
+           <HomePage/>
+       </BrowserRouter>
   )
 }
 
