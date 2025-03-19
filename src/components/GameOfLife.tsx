08c33753347cc8a94c8aed25e030d32a4e98d2e6
Chris Annunziato <cmannunziato@gmail.com>
2025-03-19 14:56:19 -0400

    added a generation

--- src/components/GameOfLife.tsx
+++ src/components/GameOfLife.tsx
@@ -2,22 +2,24 @@ import Cell from "./Cell.tsx";
 
 const GameOfLife = () => {
 
+    const generation = Array.from({length: 70}, (_, index) => (
+        <div key={index}>
+            {
+                Array.from({length: 12}, (_, index) => {
+                    return (
+                        <div key={index}>
+                            <Cell/>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    ))
+
     return (
         <section className=" w-full h-52  flex justify-center">
             {
-                Array.from({length: 70}, (_, index) => (
-                    <div key={index}>
-                        {
-                            Array.from({length: 12}, (_, index) => {
-                                return (
-                                    <div key={index}>
-                                        <Cell/>
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
-                ))
+                [...generation]
             }
         </section>
     )
