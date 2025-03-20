37950b6822b1c62a3fbdaa6cb246489710da5738
Chris Annunziato <cmannunziato@gmail.com>
2025-03-20 10:06:28 -0400

    added cellprops type

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,9 +1,13 @@
-import {useState} from "react";
+import React, {useState} from "react";
 
-const Cell = () => {
+type CellProps = {
+    cellRef : React.RefObject<HTMLDivElement | null>
+}
+
+const Cell = ({cellRef} : CellProps) => {
     const [isAlive, setIsAlive] = useState(false)
     return (
-        <div onClick={() => setIsAlive(!isAlive)} className={`h-4 w-4 bg-black border 
+        <div ref={cellRef} onClick={() => setIsAlive(!isAlive)} className={`h-4 w-4 border 
         border-gray-500 ${isAlive ? 'bg-green-300' : 'bg-black'}`}>
         </div>
     )
--- src/components/GameOfLife.tsx
+++ src/components/GameOfLife.tsx
@@ -1,6 +1,9 @@
 import Cell from "./Cell.tsx";
+import React, {useRef} from "react";
 
 const GameOfLife = () => {
+
+    const cellRef: React.RefObject<HTMLDivElement | null> = useRef(null)
 
     const generation = Array.from({length: 70}, (_, index) => (
         <div key={index}>
@@ -8,7 +11,7 @@ const GameOfLife = () => {
                 Array.from({length: 12}, (_, index) => {
                     return (
                         <div key={index}>
-                            <Cell/>
+                            <Cell cellRef={cellRef}/>
                         </div>
                     )
                 })
@@ -17,11 +20,21 @@ const GameOfLife = () => {
     ))
 
     return (
-        <section className=" w-full h-52  flex justify-center">
-            {
-                [...generation]
-            }
-        </section>
+        <div className="flex flex-col justify-center items-center">
+            <section className=" w-full h-52  flex justify-center">
+                {
+                    [...generation]
+                }
+            </section>
+            <div className="flex gap-4">
+                <button className="bg-amber-300 h-8 w-14 text-black flex justify-center items-center
+            mb-2">Start
+                </button>
+                <button className="bg-amber-300 h-8 w-14 text-black flex justify-center items-center
+            mb-2">Reset
+                </button>
+            </div>
+        </div>
     )
 }
 
