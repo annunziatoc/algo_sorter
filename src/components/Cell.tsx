0953d379c170091f6f57d8369ec56c8b5e9ed467
Chris Annunziato <cmannunziato@gmail.com>
2025-06-14 17:16:40 -0400

    redoing it, initial grid setup

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,24 +1,11 @@
-import {useEffect, useState} from "react";
 
-interface CellProps {
-    id: string;
-    updateFn: (isAlive: boolean) => boolean;
-}
 
-const Cell = ({id, updateFn}: CellProps) => {
-    const [isAlive, setIsAlive] = useState<boolean>(false)
-
-    useEffect(()=> {
-        if(isAlive){
-            updateFn(isAlive)
-        }
-    }, [isAlive])
+const Cell = () => {
 
     return (
-        <div onClick={() => {setIsAlive(!isAlive)}}
-            className={`h-4 w-4 border
-        border-gray-500 ${isAlive ? 'bg-green-300' : 'bg-black'}`}>
-        </div>
+       <div>
+
+       </div>
     )
 }
 
--- src/components/GameOfLife.tsx
+++ src/components/GameOfLife.tsx
@@ -1,57 +1,42 @@
-import Cell from "./Cell.tsx";
 import {useState} from "react";
 
 const GameOfLife = () => {
 
     const ROWS = 12
     const COLUMNS = 57
-    const [updateKey, setUpdateKey] = useState(0)
-    const [boardState, setBoardState] = useState<boolean[][]>(
-        Array.from({length: ROWS}, () => Array.from({length: COLUMNS}, () => false))
+
+    const [grid, setGrid]  = useState(
+        Array.from({length: ROWS}, (_,row) => {
+            return Array.from({length: COLUMNS}, (_,col) => {
+                const isAlive = Math.random() < 0.15 ?  1 : 0
+                return [isAlive,row,col]
+            })
+        })
     )
 
-    const resetBoardState = () => {
-        setUpdateKey((prev) => prev + 1)
-        setBoardState(Array.from({length: ROWS}, () => Array.from({length: COLUMNS}, () => false)))
-    }
+    console.log(grid)
 
-    const surroundCell = (updateKey, rowIndex, cellIndex) => {
 
-        const newBoardState = [...boardState]
 
-        newBoardState[rowIndex] = [...newBoardState[rowIndex]]
-        if(`${updateKey}-${rowIndex}-${cellIndex}`)
-        newBoardState[rowIndex][cellIndex + 1] = true
-        setBoardState(newBoardState)
-    }
-
-    // const differenceMatrix = [
-    //     [-1, -1], [-1, 0], [-1, 1],
-    //     [0, -1], [0, 0], [0, 1],
-    //     [1, -1], [1, 0], [1, 1],
-    // ]
 
 
     return (
-        <div className="min-w-[950px] ml-4 flex flex-col justify-center items-center">
-            <section className=" w-full h-52 flex flex-col">
+        <>
+            <div className="flex flex-col -ml-6 pb-3">
                 {
-                    boardState.map((row, rowIndex) => (
-                        <div key={`${rowIndex}`} className="flex">
-                            {row.map((_, cellIndex) => {
-                                return (
-                                    <Cell
-                                        key={`${updateKey}-{${rowIndex}-${cellIndex}`}
-                                        id={`${updateKey}-{${rowIndex}-${cellIndex}`}
-                                        updateFn ={surroundCell}
-                                    />
-                                )
-                            })}
+                    grid.map((row) => (
+                        <div className="flex">
+                            {row.map((arr) => (
+                                    <div className={`h-4 w-4 border border-gray-500 ${arr[0] === 1 ? 'bg-zinc-500' :  ''}`}>
+                                    </div>
+                            ))}
                         </div>
-                    ))}
-            </section>
-        </div>
+                    ))
+                }
+            </div>
+        </>
     )
+
 }
 
 
